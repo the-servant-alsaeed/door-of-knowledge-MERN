2f77148fa0379a9ed55862e5aea6b01bde91cbe8
Mohammed Alsaeed <saeedalsaeedbusiness@gmail.com>
2024-10-01 07:23:32 +0300

    adds vercel+mongoDb deployment database variable

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT=5555;
 
-export const mongoDBURL = 'mongodb+srv://root:admin@book-store-mern.0ecqd.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
+export const mongoDBURL = 'mongodb+srv://vercel-admin-user-66fb79056bcd8c3e39049ea3:HPr8nVQNDUKrCrcU@book-store-mern.0ecqd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
